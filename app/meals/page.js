c9eb24fb4bfa574f8d758ac0dffe4852efe03426
shanME <giorgie103188@gmail.com>
2024-08-30 17:15:18 +0400

    SQL

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const MealsPage = () => {
-  return <h1>Meal Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, creted{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favorite recipe and cook if uourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
